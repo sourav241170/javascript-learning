abd482b97276ff4e65d153573663fabe99737d81
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-08-27 06:12:56 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treats all js code as newer version
+
+//alert ("3 + 3") //We are using nodejs; not browser
+
+let name= "Salty" //string
+let age= 24 // number
+let isLoggedIn= false //boolean
+
+// string => ""
+// number => 2 to the power 53
+// bingint
+// boolean => true/false
+// null => standalone value 0/nothing
+// undefined => 
+// symbol => unique
+
+//object
+// typeof => defines the type
+
+console.log(typeof "Salty")
+console.log(typeof 42)
+console.log(typeof age)
