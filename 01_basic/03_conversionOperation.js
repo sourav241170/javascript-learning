453878432fd7bbe2f2c36d3a528ec4c30c86d6f2
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-08-27 15:44:59 +0000

    conversionOperation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,13 @@
+// let score="33aaa"
+// let score=null
+// let score=true
+let score=false
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score) //Convert the score to number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
