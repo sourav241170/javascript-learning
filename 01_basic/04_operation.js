2922a89bb843587a824e197c870f8af3c1a17546
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-08-30 04:58:32 +0000

    Operations

---
+++ 01_basic/04_operation.js
@@ -0,0 +1,32 @@
+let value= 3
+let negValue= -value
+
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //power
+// console.log(2/2);
+// console.log(3%2); //remainder
+
+let str1= "Hello"
+let str2= " Salty"
+
+let str3= str1 + str2
+// console.log(str3);
+
+// console.log("2" + 2);
+// console.log(2 + "2");
+// console.log("1"+ 2+ 2);
+// console.log(1 + 2 + "2"); //mess code
+
+//console.log((3+4)*5%3); //use () to explain which operation is needed first
+
+let gameCounter=100
+//gameCounter++
+++gameCounter
+
+console.log(gameCounter);
+
+
