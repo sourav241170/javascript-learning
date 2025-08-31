d91b9d5cfb42628dbeb89438e5b6091d8890b880
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-08-31 07:10:26 +0000

    Strings

---
+++ 01_basic/06_strings.js
@@ -0,0 +1,7 @@
+const name= "Sourav"
+const repoCount= 50
+
+//console.log(name+ repoCount+ " value"); OLD Method
+
+console.log(`Hello, My name is ${name} and my Repo Count is ${repoCount}`); // Always try to use this
+
