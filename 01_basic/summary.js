2c5a3f65b9480416ed5bb7c12f3e5d04b4c794c0
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-08-31 06:10:00 +0000

    Summary

---
+++ 01_basic/summary.js
@@ -0,0 +1,19 @@
+/*
+Return typeOf variables in JavaScript:
+
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+       
+       
+*/
