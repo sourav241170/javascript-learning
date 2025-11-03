b83b67d7be0026baf9b1cb9700d7cfcbb787bc03
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-11-03 04:33:08 +0000

    String

--- 01_basic/06_strings.js
+++ 01_basic/06_strings.js
@@ -5,3 +5,30 @@ const repoCount= 50
 
 console.log(`Hello, My name is ${name} and my Repo Count is ${repoCount}`); // Always try to use this
 
+const gameName= String('sourav.g')
+console.log(gameName[0]);
+console.log(gameName[2]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('v'));
+
+const newString= gameName.substring(0,5)
+console.log(newString);
+
+const anotherString= gameName.slice(-8, 2)
+console.log(anotherString);
+
+
+const removeExtra= "    SouravG    "
+console.log(removeExtra);
+console.log(removeExtra.trim());
+
+
+const url= "https://souravdigital.com/blog/atlas%20vs%20comet"
+console.log(url.replace(/%20/g, '-'));
+
+
