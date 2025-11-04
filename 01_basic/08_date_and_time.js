8bb43b066b0ae2134432a3c497fc86f0b5f58241
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-11-04 04:34:42 +0000

    Dates

---
+++ 01_basic/08_date_and_time.js
@@ -0,0 +1,23 @@
+let myDate= new Date()
+
+// console.log(typeof myDate);
+
+
+// console.log(myDate);
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+
+// let myCreatedDate= new Date(2025, 0, 15)
+// console.log(myCreatedDate.toDateString());
+
+let myCreatedDate= new Date("01-05-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp= Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
