78f90ef6f19d76dcee983b86ac922e8adbadea9a
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-11-05 04:44:35 +0000

    Arrays Part 1

---
+++ 02_basic/01_array.js
@@ -0,0 +1,40 @@
+//Arrays
+
+const myArr= [0, 1, 2, 3, 4, 5, 6, 7]
+const myHeroes= ["Ironman", "Spiderman", "Thor", "Loki"]
+
+const myArr2= new Array("Ironman", "Spiderman", "Thor", "Loki")
+
+// console.log(myArr[2]);
+// console.log(myHeroes[2]);
+// console.log(myArr2[1]);
+
+// Array Methods
+
+// myArr.push(6, 8)
+// myArr.pop()
+
+myArr.unshift(9) //add an element at the start of an array
+console.log(myArr);
+
+myArr.shift() //remove an element from the start of an array
+console.log(myArr);
+
+const newArray= myArr.join() //Changes the type
+console.log(newArray);
+console.log(typeof newArray);
+
+//Slice and Splice
+//Slice makes a copy of the mentioned range from the original Array without changing the original Array
+//Splice deletes the range from the original Array
+
+
+console.log("A", myArr);
+
+const myNew1= myArr.slice(1,5)
+console.log(myArr);
+console.log("Slice", myNew1);
+
+const myNew2= myArr.splice(1,5)
+console.log(myArr);
+console.log("Splice", myNew2);
