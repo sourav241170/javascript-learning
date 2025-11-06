c6d30c53ab0f4811ff560ca57492330d528b24a2
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-11-06 04:24:41 +0000

    Objects

---
+++ 02_basic/03_object_singletons.js
@@ -0,0 +1,33 @@
+//const tinderUser= new Object() //Singleton Object
+const tinderUser= {} //Non-Singleton Object
+
+
+tinderUser.id= "123abc"
+tinderUser.name= "Samantha"
+tinderUser.isLoggedIn= false
+
+// console.log(tinderUser);
+
+//Multiple objects inside an Object
+const regularUser= {
+    email: "samantha@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Samantha",
+            lastname: "Prabhu"
+        }
+    }
+}
+
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userfullname);
+
+const obj1= {1: "A", 2: "B"}
+const obj2= {3: "C", 4: "D"}
+
+// const obj3= Object.assign({}, obj1, obj2) //blank object is added so that all the merged objects went to the blank object and not the obj1
+
+const obj3= {...obj1, ...obj2} //mostly use this as its simple
+// console.log(obj3);
+
+
