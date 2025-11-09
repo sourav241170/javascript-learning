48a2ab32b6dabc990b32034aac885718928cf166
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-11-09 02:26:07 +0000

    Objects

--- 02_basic/03_object_singletons.js
+++ 02_basic/03_object_singletons.js
@@ -31,3 +31,26 @@ const obj3= {...obj1, ...obj2} //mostly use this as its simple
 // console.log(obj3);
 
 
+const users= [
+    {
+        id: 1,
+        email: "sg@gmail.com"
+    },
+    {
+        id: 1,
+        email: "sg@gmail.com"
+    },
+    {
+        id: 1,
+        email: "sg@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
