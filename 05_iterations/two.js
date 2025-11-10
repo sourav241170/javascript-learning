5d341f1dff70de26e3b44df0e7cc6c2e4f54601e
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-11-10 18:26:18 +0000

    While and Do While Loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,16 @@
+// while and do-while loops
+
+let myArray= ["Superman", "Batman", "Wonderwoman"]
+arr= 0
+while (arr< myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+
+
+let score= 1
+
+do {
+    console.log(`Score is: ${score}`);
+    score++
+} while (score<= 10);
