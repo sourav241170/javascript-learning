f2b68868f0ae620a951d48bd7099cadc4e12542f
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-11-10 14:03:36 +0000

    Truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,16 @@
+const userEmail= ""
+
+if(userEmail){
+    console.log("Got User Email");
+}else{
+    console.log("Dont have User Email");
+}
+
+
+// Falsy Values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Truthy value
+// Everything else which is not falsy value
+// "0", "false", " " --------> anything inside a string
+// [], {}, function(){}
