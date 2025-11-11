2a816aee9443b20257de32f2c1d945f35f31a6c0
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-11-11 03:22:21 +0000

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,26 @@
+// for of loop
+
+const arr= [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+    
+}
+
+const greetings= "Hello World!"
+for (const greet of greetings) {
+    // console.log(`Each Character is: ${greet}`);
+    
+}
+
+const map= new Map()
+map.set('IN', "India")
+map.set('Fr', "France")
+map.set('UK', "United Kingdom")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+    
+}
