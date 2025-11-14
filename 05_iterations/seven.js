ad590cc907652e4e03a63fa8272443d87c4bc482
Sourav Ghosh <178173686+sourav241170@users.noreply.github.com>
2025-11-14 03:05:17 +0000

    Reduce

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,4 +1,44 @@
-const myNumbers= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const myNumbers= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const newNums=myNumbers.map((nums)=> nums+ 10)
-console.log(newNums);
+// const newNums=myNumbers.map((nums)=> nums+ 10)
+// console.log(newNums);
+
+
+//------------ Reduce ----------------
+
+const myNums= [1, 2, 3]
+
+// const myTotal= myNums.reduce(function(accumulator, currentValue){
+//     console.log(`Accumulator: ${accumulator} and Current Value: ${currentValue}`); // Just to view the Accoumulator and Current Value
+//     return accumulator+currentValue
+// }, 0)
+
+// The same reduce function writen in arrow form-->
+const myTotal= myNums.reduce( (accumulator, currentValue) => accumulator+currentValue, 0 )
+
+// console.log(myTotal);
+
+
+const shoppingCart=[
+    {
+        itemName: "JS-Course",
+        price: 899
+    },
+    {
+        itemName: "py-Course",
+        price: 799
+    },
+    {
+        itemName: "Web Dev",
+        price: 1099
+    },
+    {
+        itemName: "App Dev",
+        price: 5099
+    }
+]
+
+const totalCart= shoppingCart.reduce ( (accumulator, item) => accumulator+item.price, 0 );
+console.log(`Total Cart Value is ${totalCart}`);
+
+
